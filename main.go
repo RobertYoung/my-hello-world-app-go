aaf0083e53b8e34a61852e4d6a176f1b39db6b2f
Robert Young <me@iamrobertyoung.co.uk>
2020-01-11 15:06:11 +0000

    Get the hostname in the handler

--- main.go
+++ main.go
@@ -8,8 +8,6 @@ import (
 	"strconv"
 )
 
-var hostname = ""
-
 const (
 	// PORT represents the listening port for the webserver
 	PORT = 8080
@@ -22,8 +20,6 @@ type EchoResponse struct {
 }
 
 func main() {
-	getHostname()
-
 	port := strconv.Itoa(PORT)
 	fmt.Printf("Listening on port " + port + "\n")
 
@@ -33,21 +29,17 @@ func main() {
 	http.ListenAndServe(":"+port, nil)
 }
 
-func getHostname() {
-	name, err := os.Hostname()
-
-	if err != nil {
-		panic(err)
-	}
-
-	hostname = name
-}
-
 func handler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Server is running!")
 }
 
 func echoHandler(w http.ResponseWriter, r *http.Request) {
+	hostname, err := os.Hostname()
+
+	if err != nil {
+		panic(err)
+	}
+
 	data := EchoResponse{
 		Path:     "/" + r.URL.Path[1:],
 		Hostname: hostname,
